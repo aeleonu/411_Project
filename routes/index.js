54b0a76a646fe171284c52aaf1314e8e97a0daf3
Azu Eleonu <aeleonu@bu.edu>
2017-12-02 14:32:18 -0500

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,33 @@ var router = express.Router();
 var request = require('request');
 
 var app = express();
-/* GET home page. */
+
+/* GET login page. */
+
+router.get('/', function(req, res, next) {
+    res.render('login', { title: 'Moodsquare' });
+});
+
+router.post('/',function(req, res, next) {
+
+    /* GET home page. */
+
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Cuisine Search' });
 });
 
-/* POST home page.*/
 
+    /* POST home page.*/
 router.post('/',function(req, res, next) {
     var searchCuisines = req.body.searchbox;
     var options = {
       url:'https://developers.zomato.com/api/v2.1/search?entity_id=289&entity_type=city&q='+String(searchCuisines),
         headers: {
           'user-key' : '2a6c5c905e4f10836e05be8dfa827278',
-
         },
         method:'GET'
     };
+
     request(options, function (error, response, body) {
 
       if (error) throw new Error(error);
@@ -41,6 +51,28 @@ router.post('/',function(req, res, next) {
       res.send(s);
       //console.log(jsonData);
     });
+
+
+
+
+    var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
+    var tone_analyzer = new ToneAnalyzerV3({
+        username: 'e97b0ab8-8b41-4a3c-b436-c56f7151df43',
+        password: 'mkQYQSHcbDb3',
+        version_date: '2016-05-19'
+    });
+
+    tone_analyzer.tone({ text: ' A scarf rears a person before an approaching workload. Inside the corridor strains a vote. Why won\'t a heresy try under the adjusted season? Our poster hurries the spiritual past the cooking era. The substitute rescue waits for an ideal. A dumb supernatural crushes the shoulder without a miraculous detector. ' },
+        function(err, tone) {
+            if (err)
+                console.log(err);
+            else
+                console.log(JSON.stringify(tone, null, 2));
+        });
+
 });
 
-module.exports = router;
+
+    module.exports = router;
+
